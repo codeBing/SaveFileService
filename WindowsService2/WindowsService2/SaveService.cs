59c83c19758f4abb3acc957c0be44117191c343e
BING <407272759@qq.com>
2015-01-28 10:40:22 +0800

    测试成功，可定时读取文件保存到数据库，并正确移动到备份文件夹，同时输出服务启动和停止时间到日志文件

--- WindowsService2/WindowsService2/SaveService.cs
+++ WindowsService2/WindowsService2/SaveService.cs
@@ -177,7 +177,7 @@ namespace WindowsService2
         private void LogMessage(string action)
         {
             string path = Directory.GetCurrentDirectory();
-            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(path+"\\log.txt", true))
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("D:\\saveFileServiceLog.txt", true))
             {
                 sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + action);
             }
