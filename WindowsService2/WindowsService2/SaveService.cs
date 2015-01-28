48cb969df5cad98d286a73622edf031ef06ea158
BING <407272759@qq.com>
2015-01-28 10:25:23 +0800

    初步合并定时保存文件功能

--- WindowsService2/WindowsService2/SaveService.cs
+++ WindowsService2/WindowsService2/SaveService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -14,7 +16,10 @@ namespace WindowsService2
     public partial class SaveService : ServiceBase
     {
         System.Timers.Timer timer1;
-        
+        static string path = ConfigurationManager.AppSettings["FileDirectory"];
+        static string backupPath = ConfigurationManager.AppSettings["BackupDirectory"];
+        static DBHelper db = new DBHelper();
+
         public SaveService()
         {
             InitializeComponent();
@@ -54,22 +59,127 @@ namespace WindowsService2
 
         private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            LogMessage("检查服务运行！");
+            saveFile();
         }
 
-        private void LogMessage(string xMsg)
+        private void saveFile()
+        {
+            string[] strFiles = Directory.GetFiles(path);
+            string[] spilt;
+            string[] spiltData;
+            string fileName;
+
+            //遍历整个文件夹
+            foreach (string strFile in strFiles)
+            {
+                //取得文件名
+                fileName = Path.GetFileName(strFile);
+                //分割文件名
+                spilt = fileName.Split('_');
+                //设备号，记录日期和阈值相同，但时间戳较新，更新对应数据
+                if (isExist(spilt) == 1)
+                {
+                    //读取指定文件第一行
+                    using (StreamReader sr = File.OpenText(strFile))
+                    {
+                        string s = "";
+                        s = sr.ReadLine();
+                        spiltData = s.Split(',');
+                        updateData(spilt, spiltData);
+                    }
+
+                }
+                //不存在相同数据，直接插入
+                if (isExist(spilt) == 0)
+                {
+                    //读取指定文件第一行
+                    using (StreamReader sr = File.OpenText(strFile))
+                    {
+                        string s = "";
+                        s = sr.ReadLine();
+                        spiltData = s.Split(',');
+                        insertData(spiltData, spilt[2]);
+                    }
+                }
+                //目标文件已存在，删除目标文件
+                if (File.Exists(backupPath + "\\" + fileName))
+                {
+                    File.Delete(backupPath + "\\" + fileName);
+                }
+                //移动源文件到备份文件夹
+                if (File.Exists(strFile) && !File.Exists(backupPath + "\\" + fileName))
+                {
+                    File.Move(strFile, backupPath + "\\" + fileName);
+                }
+            }
+        }
+
+        //查找是否存在设备号，记录日期和阈值相同的数据
+        private int isExist(string[] fileName)
+        {
+            DbCommand cmd = db.GetSqlStringCommond("SELECT * FROM `data` WHERE deviceId=" + fileName[0] + " AND date =" + fileName[1] + " AND threshold=" + fileName[3]);
+            DataTable result = db.ExecuteDataTable(cmd);
+            //如果设备号，记录日期和阈值相同
+            if (result.Rows.Count > 0)
+            {
+                DataRow row = result.Rows[0];
+                //设备号，记录日期和阈值相同，但时间戳不一样，更新对应数据
+                if (string.Compare(row[3].ToString(), fileName[2]) < 0)
+                {
+                    return 1;
+                }
+                return -1;
+            }
+            //不存在相同数据，直接插入
+            else
+            {
+                return 0;
+            }
+        }
+
+        //设备号，记录日期和阈值相同，但时间戳不一样，更新对应数据
+        private bool updateData(string[] fileName, string[] data)
         {
             try
             {
-                DBHelper db = new DBHelper();
-                //这里向数据库中插入一条信息为 xMsg的记录，下边是我调用事先写好的Db类添加记录的方法，您也可以使用其他办法来写入数据库。
-                //Db.QuerySQL("Insert into SysMsg (SysMsg) values ('"+xMsg+"')");
-                DbCommand cmd = db.GetSqlStringCommond("insert into testservice values(null,'" + xMsg + "')");
+                DbCommand update = db.GetSqlStringCommond("update data set timestamp=" + fileName[2] + ",value1=" + data[4] + ",value2=" + data[5] + ",value3=" + data[6] + ",value4=" + data[7] + ",value5=" + data[8] +
+                                     ",value6=" + data[9] + ",value7=" + data[10] + ",value8=" + data[11] + ",value9=" + data[12] + ",value10=" + data[13] + ",value11=" + data[14] + ",value12=" + data[15] + ",value13=" + data[16] +
+                                     ",value14=" + data[17] + ",value15=" + data[18] + ",value16=" + data[19] + ",value17=" + data[20] + ",value18=" + data[21] + ",value19=" + data[22] + ",value20=" + data[23] + ",value21=" + data[24] +
+                                     ",value22=" + data[25] + ",value23=" + data[26] + ",value24=" + data[27] + " WHERE deviceId=" + fileName[0] + " AND date =" + fileName[1] + " AND threshold=" + fileName[3]);
+                db.ExecuteNonQuery(update);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //不存在相同数据，直接插入
+        private bool insertData(string[] data, string timestamp)
+        {
+            try
+            {
+                DbCommand cmd = db.GetSqlStringCommond("insert into data values(null," + data[0] + "," +
+                            data[1] + data[2] + data[3] + "," + timestamp + "," + data[28] + "," + data[4] + "," + data[5] + "," + data[6] + "," + data[7] + "," + data[8] +
+                            "," + data[9] + "," + data[10] + "," + data[11] + "," + data[12] + "," + data[13] + "," + data[14] + "," + data[15] + "," + data[16] +
+                            "," + data[17] + "," + data[18] + "," + data[19] + "," + data[20] + "," + data[21] + "," + data[22] + "," + data[23] + "," + data[24] +
+                            "," + data[25] + "," + data[26] + "," + data[27] + ")");
                 db.ExecuteNonQuery(cmd);
             }
             catch
             {
-                //不做任何操作
+                return false;
+            }
+            return true;
+        }
+
+        private void LogMessage(string action)
+        {
+            string path = Directory.GetCurrentDirectory();
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(path+"\\log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + action);
             }
         }
     }
